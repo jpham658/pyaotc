cc5760ba8ecbbfe315be4f0057297b6b52b3ad9f
Jenny Pham <jpham658@gmail.com>
2025-04-02 17:38:15 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,11 +22,11 @@ else
     echo "Clang 14 is already installed."
 fi
 
-if ! is_installed "libpolly-14-dev"; then
-    echo "Polly 14 not found, installing..."
+if ! is_installed "llvm-14-tools"; then
+    echo "LLVM Tools 14 not found, installing..."
     sudo apt install -y libpolly-14-dev
 else
-    echo "Polly 14 is already installed."
+    echo "LLVM Tools 14 is already installed."
 fi
 
 # Set up alternatives to point to LLVM-14 and Clang-14 if needed
@@ -34,9 +34,14 @@ echo "Setting up llvm-config and clang alternatives..."
 sudo update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-14 100
 sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-14 100
 
-echo "Verifying LLVM 14, Clang 14, and libpolly-14-dev installation..."
+echo "Verifying LLVM 14, Clang 14, and LLVM Tools 14 installation..."
 llvm-config --version
 clang --version
-dpkg -l | grep libpolly-14-dev
+dpkg -l | grep 
 
 echo "Setup complete!"
+
+# Gotta figure out if this even works to set everything up...
+# wget https://apt.llvm.org/llvm.sh
+# chmod +x llvm.sh
+# sudo ./llvm.sh 14 all
--- test.py
+++
@@ -1,8 +0,0 @@
-def f(x):
-    if x == 0:
-        return "hi"
-    else:
-        return 1
-
-f(0)
-f(1)
